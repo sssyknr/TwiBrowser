caa6ec86f0c82dc595ccfc91ce2ec6ad09fae0bf
佐々木將也 <sskyknr.nn@gmail.com>
2016-10-03 01:13:57 +0900

    基盤

---
+++ TwiBrowser/app/src/androidTest/java/ss/ss/sss/twibrowser/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ss.ss.sss.twibrowser;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ TwiBrowser/app/src/main/java/ss/ss/sss/twibrowser/SplashActivity.java
@@ -0,0 +1,13 @@
+package ss.ss.sss.twibrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
